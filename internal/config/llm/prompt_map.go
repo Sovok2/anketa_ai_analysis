4cb1a14c8944a3becd47b845d2f23a62e570a9fd
syn_q <syn_q@vm88432.vps.client-server.site>
2025-11-13 11:03:12 +0100

    add prompts for another ankets

---
+++ internal/config/llm/prompt_map.go
@@ -0,0 +1,12 @@
+package llm
+
+import (
+	"anketa_ai_analysis/config/llm/prompt"
+)
+
+prompt_map := map[string]string {
+	"diviant": prompt.
+	"adaptability_monitoring": prompt,
+	"tolerance_index": prompt,
+
+}
